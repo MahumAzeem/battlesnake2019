4f1d83c8f5fdb5e01fec386a01c4decf2db2e483
Jeffrey Li <jffr.li1@gmail.com>
2019-02-25 19:24:36 -0800

    More attempts at not breaking things

--- src/test/java/io/battlesnake/starter/SColour.java
+++ src/test/java/io/battlesnake/starter/SColour.java
@@ -1,12 +1,10 @@
 package io.battlesnake.starter;
 
 public class SColour {
-	String colour = "#54d396";
-
 	SColour() {
 
 	}
 	public static String getC() {
-		return this.colour;
+		return "#54d396";;
 	}
 }
