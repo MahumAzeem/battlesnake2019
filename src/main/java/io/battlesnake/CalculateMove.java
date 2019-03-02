28f52347e719bb4716260fecf1c63e119c678a4a
Jeffrey Li <jffr.li1@gmail.com>
2019-03-02 10:34:28 -0800

    Thing

--- src/main/java/io/battlesnake/CalculateMove.java
+++ src/main/java/io/battlesnake/CalculateMove.java
@@ -1,7 +1,7 @@
 package io.battlesnake.starter;
 import java.util.ArrayList;
 
-public class CalculateMove(GetData dataparser){
+public class CalculateMove{
   public String nextMove;
 
   public CalculateMove(GetData dataparser){
