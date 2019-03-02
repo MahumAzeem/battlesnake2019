b44c54902cdd28b9b6abfb9546e32e6f5bc753bb
Jeffrey Li <jffr.li1@gmail.com>
2019-03-02 11:43:24 -0800

    Debug

--- src/main/java/io/battlesnake/GetData.java
+++ src/main/java/io/battlesnake/GetData.java
@@ -54,7 +54,7 @@ public class GetData {
 	}
 
 	private void initsnakes(JsonNode data) {
-		JsonNode enemies = data.get.("board").get("snakes");
+		JsonNode enemies = data.get("board").get("snakes");
 		snakes = new Snek[enemies.size()];
 		for(int i = 0; i < enemies.size(); i++) {
 			snakes[i] = initsnake(enemies.get(i));
