ae53e427a2f454acec264794c31f91594f9996f5
MahumAzeem <azeem.mahum@gmail.com>
2019-03-02 10:52:39 -0800

    error fix

--- src/main/java/io/battlesnake/CalculateMove.java
+++ src/main/java/io/battlesnake/CalculateMove.java
@@ -2,7 +2,7 @@ package io.battlesnake.starter;
 import java.util.ArrayList;
 
 public class CalculateMove{
-  protected String nextMove;
+  public String nextMove;
 
   public CalculateMove(GetData dataparser){
     nextMove = "down";
