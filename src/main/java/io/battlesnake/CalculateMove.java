915c13141d81f589ba1ee0312bb3835a9a44dd57
Jeffrey Li <jffr.li1@gmail.com>
2019-03-02 14:42:00 -0800

    Foodchaser?

--- src/main/java/io/battlesnake/CalculateMove.java
+++ src/main/java/io/battlesnake/CalculateMove.java
@@ -97,6 +97,96 @@ public class CalculateMove{
   	nextMove = "up";
   }
 */
+  public String foodChaser() {
+    Snek you = data.you;
+    int foodind = 0;
+    int mindis = 9999;
+    XY[] food = data.food;
+    for (int i = 0; i < food.length; i++) {
+      if (you.getHead().getDistance(food[i]) < mindis) {
+        foodind = i;
+      }
+    }
+    if (food.length > 0) return foodChaser(food[foodind]);
+
+    XY headpos = you.getHead();
+    int ww = wallWarning(headpos, data.width, data.height);
+    if (ww == 1 || ww == 2) {
+      if (data.occupied(new XY(headpos.getx(), headpos.gety() -1 )) || data.danger(new XY(headpos.getx(), headpos.gety() -1 ))) {
+        return "down";
+      }
+      else {
+        return "up";
+      }
+    }
+    if (ww == 10 || ww == 20) {
+      if (data.occupied(new XY(headpos.getx()-1, headpos.gety())) || data.danger(new XY(headpos.getx()-1, headpos.gety())) ) {
+        return "left";
+      }
+      else {
+        return "right";
+      }
+    }
+    if (ww == 11) {
+      if (data.occupied(new XY(headpos.getx()+1, headpos.gety())) || data.danger(new XY(headpos.getx()+1, headpos.gety()))) {
+        return "down";
+      }
+      else {
+        return "right";
+      }
+    }
+    if (ww == 12) {
+      if (data.occupied(new XY(headpos.getx()-1, headpos.gety())) || data.danger(new XY(headpos.getx()-1, headpos.gety()))) {
+        return "down";
+      }
+      else {
+        return "left";
+      }
+    }
+    if (ww == 21) {
+      if (data.occupied(new XY(headpos.getx()+1, headpos.gety())) || data.danger(new XY(headpos.getx()+1, headpos.gety()))) {
+        return "up";
+      }
+      else {
+        return "right";
+      }
+    }
+    if (ww == 22) {
+      if (data.occupied(new XY(headpos.getx()+1, headpos.gety())) || data.danger(new XY(headpos.getx()+1, headpos.gety()))) {
+        return "up";
+      }
+      else {
+        return "left";
+      }
+    }
+    return null; //go forward?
+  }
+
+  private String foodChaser(XY food) {
+    XY pos = snake.getHead();
+    int x = food.getx() - pos.getx();
+    if (x > 0) {
+      XY check = new XY(pos.getx()+1, pos.gety());
+      if (!data.occupied(check) && !data.danger(check)) {
+        return "right";
+      }
+    }
+    else if (x < 0) {
+      XY check = new XY(pos.getx()-1, pos.gety());
+      if (!data.occupied(check) && !data.danger(check)) {
+        return "left";
+      }
+    }
+    x = food.gety() - pos.gety();
+    if (x > 0) {
+      XY check = new XY(pos.getx(), pos.gety()+1);
+      if (!data.occupied(check) && !data.danger(check)) {
+        return "down";
+      }
+    }
+    return "up";
+  }
+
   public int wallWarning(XY head, int width, int height) {
   	int x = 1; //1-left wall, 2-right wall
   	int y = 10;//1-up wall, 2-down wall
